e6af74218ee30020afe111baf01d806c8fae9bfe
Tristan King <tristan@bakkenbaeck.no>
2017-12-20 17:41:38 +0100

    add gas estimate buffer

--- toshieth/jsonrpc.py
+++ toshieth/jsonrpc.py
@@ -151,6 +151,8 @@ class ToshiEthJsonRPC(JsonRPCBase, BalanceMixin, DatabaseMixin, EthereumMixin, A
                 # this can occur if sending a transaction to a contract that doesn't match a valid method
                 # and the contract has no default method implemented
                 raise JsonRPCInvalidParamsError(data={'id': 'invalid_data', 'message': 'Unable to estimate gas for contract call'})
+            # buffer gas estimate by 20%
+            gas *= 1.2
         else:
             gas = parse_int(gas)
             if gas is None:
